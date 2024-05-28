9daa189207358207d3cdf89e671bf172fdea8886
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-05-27 23:31:47 -0300

    HEADER DONE / FAVICON DONE / GLOBAL FONTFAMILY DONE

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,8 +1,29 @@
+import Nav from "./nav/Nav";
+import logoHeader from "./logo.svg";
+import style from "./Header.module.css"
+import { useLocation } from "react-router-dom";
+
 const Header = () => {
 
+    const location = useLocation();
+
+    let headerClass;
+    if (location.pathname === "/") {
+        headerClass = style.home;
+    } else if (location.pathname === "/servicios") {
+        headerClass = style.services;
+    } else if (location.pathname === "/contacto") {
+        headerClass = style.contact;
+    } else {
+        headerClass = "";
+    }
+
     return (
-        <div>
-            Soy el Header
+        <div className={`${style.div} ${headerClass}`}>
+            <div className={style.content}>
+                <h1><img src={logoHeader} alt="Catalá Hanserik - Logo" title="Catalá Hanserik - Abogadas"/></h1>
+                <div className={style.nav}><Nav/></div>
+            </div>
         </div>
     );
 }
---
+++ src/components/common/header/nav/Nav.jsx
@@ -0,0 +1,31 @@
+import { NavLink, useLocation } from "react-router-dom";
+import style from './Nav.module.css';
+
+const Nav = () => {
+    const location = useLocation();
+
+    return (
+        <div>
+            <NavLink
+                className={`${style.NavLink} ${location.pathname === '/' ? style.selected : ''}`}
+                to="/"
+            >
+                <span className={style.dot}>•</span> ‎ ‎Inicio‎ ‎ <span className={style.dot}>•</span>
+            </NavLink>
+            <NavLink
+                className={`${style.NavLink} ${location.pathname === '/servicios' ? style.selected : ''}`}
+                to="/servicios"
+            >
+                <span className={style.dot}>•</span> ‎ ‎Servicios‎ ‎ <span className={style.dot}>•</span>
+            </NavLink>
+            <NavLink
+                className={`${style.NavLink} ${location.pathname === '/contacto' ? style.selected : ''}`}
+                to="/contacto"
+            >
+                <span className={style.dot}>•</span> ‎ ‎Contacto‎ ‎ <span className={style.dot}>•</span>
+            </NavLink>
+        </div>
+    );
+}
+
+export default Nav;
