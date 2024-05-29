2722bb1b5da319a4d0fbd2d7aae9b83d570b4b9d
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-05-28 22:47:35 -0300

    footer FIXED/services page DONE

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,24 +1,45 @@
-import style from "./Footer.module.css";
-import adress from "./img/adress.svg";
-import email from "./img/email.svg";
-import whatsapp from "./img/whatsapp.svg";
-import instagram from "./img/instagram.svg";
-import { useLocation } from "react-router-dom";
+import { useState } from 'react';
+import style from './Footer.module.css';
+import adress from './img/adress.svg';
+import email from './img/normal/email.svg';
+import whatsapp from './img/normal/whatsapp.svg';
+import instagram from './img/normal/instagram.svg';
+import emailHover from './img/hover/email.svg';
+import whatsappHover from './img/hover/whatsapp.svg';
+import instagramHover from './img/hover/instagram.svg';
 
 const Footer = () => {
-
-    const location = useLocation();
+    const [emailSrc, setEmailSrc] = useState(email);
+    const [whatsappSrc, setWhatsappSrc] = useState(whatsapp);
+    const [instagramSrc, setInstagramSrc] = useState(instagram);
 
     return (
-        <div className={`${style.div} ${location.pathname === '/servicios' ? style.fixed : ''}`}>
+        <div className={style.div}>
             <div className={style.content}>
                 <div className={style.location}>
-                    <p><img src={adress} alt=""/><a href="https://maps.app.goo.gl/FSziRstVoScLMw7G9" target="_blank">Belgrano, Ciudad Autónoma de Buenos Aires</a></p>
+                    <p>
+                        <img src={adress} alt="direccion"/>
+                        <a href="https://maps.app.goo.gl/FSziRstVoScLMw7G9" target="_blank" rel="noopener noreferrer">
+                            Belgrano, Ciudad Autónoma de Buenos Aires
+                        </a>
+                    </p>
                 </div>
                 <div className={style.social}>
-                    <a href=""><img src={email} alt=""/></a>
-                    <a href=""><img src={whatsapp} alt=""/></a>
-                    <a href=""><img src={instagram} alt=""/></a>
+                    <a href="" target='_blank'
+                       onMouseEnter={() => setEmailSrc(emailHover)}
+                       onMouseLeave={() => setEmailSrc(email)}>
+                        <img src={emailSrc} alt="email"/>
+                    </a>
+                    <a href="" target='_blank'
+                       onMouseEnter={() => setWhatsappSrc(whatsappHover)}
+                       onMouseLeave={() => setWhatsappSrc(whatsapp)}>
+                        <img src={whatsappSrc} alt="whatsapp"/>
+                    </a>
+                    <a href="" target='_blank'
+                       onMouseEnter={() => setInstagramSrc(instagramHover)}
+                       onMouseLeave={() => setInstagramSrc(instagram)}>
+                        <img src={instagramSrc} alt="instagram"/>
+                    </a>
                 </div>
             </div>
         </div>
--- src/components/views/services/Services.jsx
+++ src/components/views/services/Services.jsx
@@ -1,8 +1,46 @@
+import { useState } from 'react';
+import normal1 from './img/normal/1_normal.svg'
+import normal2 from './img/normal/2_normal.svg'
+import normal3 from './img/normal/3_normal.svg'
+import normal4 from './img/normal/4_normal.svg'
+import normal5 from './img/normal/5_normal.svg'
+import selected1 from './img/selected/1_selected.svg'
+import selected2 from './img/selected/2_selected.svg'
+import selected3 from './img/selected/3_selected.svg'
+import selected4 from './img/selected/4_selected.svg'
+import selected5 from './img/selected/5_selected.svg'
+import style from './Services.module.css';
+
 const Services = () => {
+    const [selectedItem, setSelectedItem] = useState(0);
+
+    const items = [
+        { buttonImgNormal: normal1, buttonImgSelected: selected1, buttonText: "AMPAROS DE SALUD", content: "1. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed " },
+        { buttonImgNormal: normal2, buttonImgSelected: selected2, buttonText: "DEFENSA DEL CONSUMIDOR", content: "2. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed " },
+        { buttonImgNormal: normal3, buttonImgSelected: selected3, buttonText: "ACCIDENTES DE TRABAJO", content: "3. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed " },
+        { buttonImgNormal: normal4, buttonImgSelected: selected4, buttonText: "GESTIÓN DE CIUDADANÍA", content: "4. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed " },
+        { buttonImgNormal: normal5, buttonImgSelected: selected5, buttonText: "PEQUEÑAS EMPRESAS", content: "5. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed " }
+    ];
 
     return (
-        <div>
-            Soy el Services
+
+        <div className={style.div}>
+            <div className={style.content}>
+                <div className={style.sidebar}>
+                    {items.map((item, index) => (
+                        <button
+                            key={index}
+                            className={`${style.menuItem} ${selectedItem === index ? style.active : ''}`}
+                            onClick={() => setSelectedItem(index)}
+                        >
+                            <p><img src={`${selectedItem === index ? item.buttonImgSelected : item.buttonImgNormal}`} alt=""/>{item.buttonText}</p>
+                        </button>
+                    ))}
+                </div>
+                <div className={style.container}>
+                    <p>{items[selectedItem].content}</p>
+                </div>
+            </div>
         </div>
     );
 }
