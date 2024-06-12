927c4176c1daef871151d77d57ab6c1e326ce38a
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2024-06-12 17:07:43 -0300

    about responsive DONE

--- src/components/views/home/about/About.jsx
+++ src/components/views/home/about/About.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import style from "./About.module.css";
 import catala from "./img/catala.jpg";
 import hanseryk from "./img/hanseryk.jpg";
 
 const About = () => {
     const lineRefs = [useRef(null), useRef(null), useRef(null)];
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 720);
 
     useEffect(() => {
         const observer = new IntersectionObserver(entries => {
@@ -32,33 +33,57 @@ const About = () => {
     }, []);
 
     return (
-        <div className={style.div}>
-            <section className={style.catala}>
-                <div className={style.content}>
+        <div>
+            {isMobile ? (
+                <div className={style.div}>
+                    <div className={style.content}>
                     <div className={style.title}>
                         <div className={style.line1}></div>
                         <h2>NOSOTRAS</h2>
                         <div className={style.line} ref={lineRefs[0]}></div>
                     </div>
-                    <div className={style.withMargin}>
+                    <section className={style.catala}>
                         <div className={style.profileImg}><img src={catala} alt="Samanta Catalá"/></div>
                         <h3>SAMANTA CATALÁ</h3>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                    </div>
-                </div>
-            </section>
-            <section className={style.hanseryk}>
-                <div className={style.content}>
-                    <div className={style.title}>
-                        <div className={style.line} ref={lineRefs[1]}></div>
-                    </div>
-                    <div className={style.withMargin}>
+                    </section>
+                    <section className={style.hanseryk}>
                         <div className={style.profileImg}><img src={hanseryk} alt="Cecilia Hanseryk"/></div>
                         <h3>CECILIA HANSERYK</h3>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    </section>
                     </div>
                 </div>
-            </section>
+            ) : (
+                <div className={style.div}>
+                    <section className={style.catala}>
+                        <div className={style.content}>
+                            <div className={style.title}>
+                                <div className={style.line1}></div>
+                                <h2>NOSOTRAS</h2>
+                                <div className={style.line} ref={lineRefs[0]}></div>
+                            </div>
+                            <div className={style.withMargin}>
+                                <div className={style.profileImg}><img src={catala} alt="Samanta Catalá"/></div>
+                                <h3>SAMANTA CATALÁ</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={style.hanseryk}>
+                        <div className={style.content}>
+                            <div className={style.title}>
+                                <div className={style.line} ref={lineRefs[1]}></div>
+                            </div>
+                            <div className={style.withMargin}>
+                                <div className={style.profileImg}><img src={hanseryk} alt="Cecilia Hanseryk"/></div>
+                                <h3>CECILIA HANSERYK</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            )}
         </div>
     );
 }
--- src/components/views/home/servicesHome/ServicesHome.jsx
+++ src/components/views/home/servicesHome/ServicesHome.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import style from "./ServicesHome.module.css";
 import normal1 from '../../services/img/normal/1_normal.svg';
 import normal2 from '../../services/img/normal/2_normal.svg';
@@ -8,6 +8,7 @@ import normal5 from '../../services/img/normal/5_normal.svg';
 
 const ServicesHome = () => {
     const titleRef = useRef(null);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 720);
 
     useEffect(() => {
         const observer = new IntersectionObserver(entries => {
@@ -30,38 +31,55 @@ const ServicesHome = () => {
         };
     }, []);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 720);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div className={style.div}>
-            <div className={style.content}>
-                <section className={style.title} ref={titleRef}>
-                    <h2>SERVICIOS</h2>
-                </section>
-                <section className={style.service}>
-                    <img src={normal1} alt=""/>
-                    <h3>AMPAROS DE SALUD</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
-                </section>
-                <section className={style.service}>
-                    <img src={normal2} alt=""/>
-                    <h3>DEFENSA DEL CONSUMIDOR</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
-                </section>
-                <section className={style.service}>
-                    <img src={normal3} alt=""/>
-                    <h3>ACCIDENTES DE TRABAJO</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
-                </section>
-                <section className={style.service}>
-                    <img src={normal4} alt=""/>
-                    <h3>GESTIÓN DE CIUDADANÍA</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p></section>
-                <section className={style.service}>
-                    <img src={normal5} alt=""/>
-                    <h3>PEQUEÑAS EMPRESAS</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
-                </section>
-            </div>
+        <div>
+            {isMobile ? (
+                <div></div>
+            ) : (
+                <div className={style.div}>
+                    <div className={style.content}>
+                        <section className={style.title} ref={titleRef}>
+                            <h2>SERVICIOS</h2>
+                        </section>
+                        <section className={style.service}>
+                            <img src={normal1} alt=""/>
+                            <h3>AMPAROS DE SALUD</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+                        </section>
+                        <section className={style.service}>
+                            <img src={normal2} alt=""/>
+                            <h3>DEFENSA DEL CONSUMIDOR</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+                        </section>
+                        <section className={style.service}>
+                            <img src={normal3} alt=""/>
+                            <h3>ACCIDENTES DE TRABAJO</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+                        </section>
+                        <section className={style.service}>
+                            <img src={normal4} alt=""/>
+                            <h3>GESTIÓN DE CIUDADANÍA</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p></section>
+                        <section className={style.service}>
+                            <img src={normal5} alt=""/>
+                            <h3>PEQUEÑAS EMPRESAS</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat</p>
+                        </section>
+                    </div>
+                </div>
+            )}
         </div>
+        
     );
 };
 
